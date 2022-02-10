052a168f61bf492b07ee0269d7284b1835b20d31
Pineapple518 <virajlalsare9@gmail.com>
2022-02-10 21:34:22 +0530

    alpha version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function change_to_home(){
+  window.location.href = "../index.html";
+}
+
+function change_to_about(){
+  window.location.href = "../pages/About.html";
+}
+
+function change_to_contact(){
+  window.location.href = "../pages/Contact.html";
+}
+
+function change_to_blogs(){
+  window.location.href = "../pages/Blogs.html";
+}
