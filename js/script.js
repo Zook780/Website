2b81ba0e56ec2d7a6fc44802b2ae7b5a62122600
Zook780 <virajlalsare9@gmail.com>
2022-03-05 21:20:55 +0530

    website redsign

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+function change_to_home(){
+    window.location.href = "index.html";
+}
+
+function change_to_musicli(){
+    window.location.href = "./pages/musiclist.html";
+}    
+
+function change_to_about(){
+    window.location.href = "./pages/about.html";
+}
+
+function change_to_contact(){
+    window.location.href = "./pages/contact.html";
+}
+
+function change_to_blogs(){
+    window.location.href = "./pages/blogs.html";
+}
+
